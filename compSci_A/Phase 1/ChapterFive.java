7fb2c4716fa621d5b8f8ac16b95d7822790ab121
SOFIA\solom <tinytitan2138@gmail.com>
2022-11-07 18:09:12 -0800

    All that was done for the day, so not much.

--- compSci_A/Phase 1/ChapterFive.java
+++ compSci_A/Phase 1/ChapterFive.java
@@ -1,5 +1,28 @@
 public class ChapterFive {
     public static void main(String[] args){
+    /*
+    Some important aspects of coding is actual program design itself. One design technique is called the
+    waterfall model. It is visualized as analysis of specification at the top, with a line connecting
+    it to program design, then implementation, then testing and debugging, and finally maintenance.
+    Analysis of specification is simply the description of the problem itself, constraints and otherwise.
+    Program design is simply the design of the solution, it can be thought of as a plan to achieve
+    whatever the goal is, it includes the methods, techniques, objects, etc. Program implementation is
+    simply the coding part. The testing part is just testing out all the cases of the program,
+    it may be to add in negative values if the program only works for positive numbers etc. The testing
+    phase also involved resolving errors, they can be compiled time errors, errors in which certain lines
+    of code cant be compiled into byte code, run time errors in which they occur during the execution of the
+    program, like index out of bounds or basically anything with an exception call, and finally the hardest being
+    logic errors, in which the code executes, but it still doesn't do what was intended. Good programs
+    account for user error, I personally just like to use custom-made exceptions and exception handling.
+    Another important process is upgrading, like adding new features to the code and what not. OOP basically
+    follows this:
+    • Identify classes to be written.
+    • Identify behaviors (i.e., methods) for each class.
+    • Determine the relationships between classes.
+    • Write the interface (public method headers) for each class.
+    • Implement the methods
 
+
+     */
     }
-} //223
+}
