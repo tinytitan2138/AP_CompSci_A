a7ef51daf5ac6154d5070d45c8a82a7c7f5710c3
SOFIA\solom <tinytitan2138@gmail.com>
2022-11-12 09:34:26 -0800

    Chapter 9 covered The Picture Lab, the Elevens Lab, and the Magpie Lab. I will do the implementation of these labs during Phase 2. This marks the end of Phase 1 for AP Computer Science A. I will now move on to the other AP classes then return to do Phase 2.

---
+++ compSci_A/Phase 1/ChapterNine.java
@@ -0,0 +1,50 @@
+public class ChapterNine {
+    public static void main(String[] args){
+        /*
+        AP Computer Science A has different types of labs, there may be questions regarding
+        these labs on the test. One such lab is called the magpie lab, it is a chat bot which students
+        modify. You would often use string methods, some important skills to have been: count the
+        number of occurrences of a substring within a string, replace all substrings with another,
+        and to remove all substring within a string. These are important to know for the lab as the lab
+        needs you to search for occurrences within a string to formulate a response. Another important
+        lab is the elevens lab, it is essentially a card playing game, it can be solitaire or something else.
+        Important components are the cards, the deck of cards, the board itself which is inherited from the
+        board class (or implemented), and finally the shuffle function. I will note that the shuffle function
+        makes some card within the array randomly one of the other cards, this prevents duplication. A deck
+        of cards has 52 cards. A lot of emphasis is put on testing and debugging within the elevens lab;
+        I will also note that you need to simulate random events, e.g. tossing a coin. I note that for the
+        elevens lab and this other lab, the picture lab, you need a GUI, but this GUI is provided, and you
+        will not be tested on it within the AP exam. The picture lab basically involves manipulating an
+        image that is modeled with a 2-d array. Array traversals are then important, same with UML diagrams,
+        these diagrams essentially help software engineers model different aspects of their software and how
+        they relate to each other. Here are some important aspects of the picture lab: • Learning how colors are stored in a program.
+
+        • Modifying a picture.
+        • Creating a mirror image of a picture.
+        • Mirroring part of a picture.
+        • Creating a collage.
+        • Detecting the edge of a picture.
+
+         The actual implementation of these projects will be done by me during Phase 2. These
+         projects are not that trivial, and thus they constitute being an actual problem.
+
+         */
+    }
+}
+/*
+Chapter 9 summary:
+String manipulation and matrix processing are the two big topics you should master.
+Review the meanings and boundary conditions of the parameters in the String methods substring and indexOf. For matrices, you should nail down both the row-column
+and for-each traversals. Remember, you cannot use a for-each loop for the replacement
+of elements.
+
+Be sure you can hand-execute tricky matrix algorithms, like those used for modifying matrices using mirror images.
+A matrix is an array of row-arrays, so familiarize yourself with the use of a
+method with an array parameter to process the rows of a matrix.
+Array manipulation is another big topic. Be sure you know how to shuffle the
+elements of an array.
+Other concepts emphasized in the labs are inheritance and polymorphism, writing
+subclasses, abstract classes, simulation of events using random numbers, multi-base
+numbers, and conditional (if...else) statements. You should have all of these at
+your fingertip
+ */
